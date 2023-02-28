9b1193f968ece95453578cd683ef6ea57c36366f
dvolper <github@david.vollmers.org>
2023-02-28 20:06:29 +0100

    run prettier

--- docs/tailwind.config.cjs
+++ docs/tailwind.config.cjs
@@ -1,10 +1,10 @@
-const defaultTheme = require( 'tailwindcss/defaultTheme' )
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  content: [ './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}' ],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: [ 'Inter var', ...defaultTheme.fontFamily.sans ],
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
   },
