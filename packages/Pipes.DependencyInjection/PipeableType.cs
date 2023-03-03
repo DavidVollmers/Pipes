2ddace43dad4873a4fb73acbc0165cde3640bf9a
dvolper <github@david.vollmers.org>
2023-03-03 12:38:33 +0100

    refactor pipeable type implementation

--- packages/Pipes.DependencyInjection/PipeableType.cs
+++ packages/Pipes.DependencyInjection/PipeableType.cs
@@ -6,8 +6,10 @@ namespace Pipes.DependencyInjection;
 internal class PipeableType : IPipeable<object, object>
 {
     private readonly ServiceInjection _serviceInjection;
-    private IPipeable<object, object>? _pipeable;
-    
+    private readonly Type _inputType;
+    private readonly Type _outputType;
+
+    private object? _pipeable;
     private IServiceProvider? _serviceProvider;
 
     public Type Type { get; }
@@ -16,23 +18,40 @@ internal class PipeableType : IPipeable<object, object>
     {
         Type = type;
         _serviceInjection = serviceInjection;
+
+        var pipeableInterface = Type.GetInterface(typeof(IPipeable<,>).Name)!;
+        _inputType = pipeableInterface.GenericTypeArguments[0];
+        _outputType = pipeableInterface.GenericTypeArguments[1];
     }
 
     public object? ConvertInput(object? input)
     {
         if (_serviceInjection == ServiceInjection.OnInput) ActivateType();
 
-        return _pipeable!.ConvertInput(input);
+        var convertInputMethod = Type.GetMethod(nameof(ConvertInput))!;
+        return convertInputMethod.Invoke(_pipeable, new[] { input });
     }
 
     public void Execute(IPipe<object, object?> pipe)
     {
-        _pipeable!.Execute(pipe);
+        var genericPipe = CreateGenericPipeImplementation(pipe);
+
+        var executeMethod = Type.GetMethod(nameof(Execute));
+        executeMethod!.Invoke(_pipeable, new[] { genericPipe });
     }
 
     public Task ExecuteAsync(IPipe<object, object?> pipe, CancellationToken cancellationToken = default)
     {
-        return _pipeable!.ExecuteAsync(pipe, cancellationToken);
+        var genericPipe = CreateGenericPipeImplementation(pipe);
+
+        var executeAsyncMethod = Type.GetMethod(nameof(ExecuteAsync));
+        return (Task)executeAsyncMethod!.Invoke(_pipeable, new[] { genericPipe, cancellationToken })!;
+    }
+
+    private object CreateGenericPipeImplementation(IPipe<object, object> pipe)
+    {
+        var genericPipeType = typeof(GenericPipeImplementation<,>).MakeGenericType(_inputType, _outputType);
+        return Activator.CreateInstance(genericPipeType, pipe)!;
     }
 
     public void Activate(IServiceProvider serviceProvider)
@@ -50,6 +69,6 @@ internal class PipeableType : IPipeable<object, object>
 
     private void ActivateType()
     {
-        _pipeable = (dynamic)_serviceProvider!.GetRequiredService(Type);
+        _pipeable = _serviceProvider!.GetRequiredService(Type);
     }
 }
