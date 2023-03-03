c9da851ccfe55da896826d2456470ccbbb3d116f
dvolper <github@david.vollmers.org>
2023-03-03 19:14:52 +0100

    caching utils

--- packages/Pipes.DependencyInjection/Caching/Cache.cs
+++ packages/Pipes.DependencyInjection/Caching/Cache.cs
@@ -7,6 +7,7 @@ public static class Cache
 {
     private static IPipeable<object, object> CacheService(Type type, CacheFlags cacheFlags)
     {
+        if (type == null) throw new ArgumentNullException(nameof(type));
         var pipeableInterface = type.GetInterface(typeof(IPipeable<,>).Name);
         if (pipeableInterface == null) throw new Exception("Type must be assignable to IPipeable.");
         return new PipeableCache<object, object>(new PipeableType(type, pipeableInterface), cacheFlags);
@@ -25,5 +26,23 @@ public static class Cache
     public static IPipeable<object, object> Everything<TType>()
     {
         return CacheService(typeof(TType), CacheFlags.Input & CacheFlags.Output);
+    }
+    
+    public static IPipeable<TInput, TOutput> Output<TInput, TOutput>(IPipeable<TInput, TOutput> pipeable)
+    {
+        if (pipeable == null) throw new ArgumentNullException(nameof(pipeable));
+        return new PipeableCache<TInput, TOutput>(pipeable, CacheFlags.Output);
+    }
+
+    public static IPipeable<TInput, TOutput> Input<TInput, TOutput>(IPipeable<TInput, TOutput> pipeable)
+    {
+        if (pipeable == null) throw new ArgumentNullException(nameof(pipeable));
+        return new PipeableCache<TInput, TOutput>(pipeable, CacheFlags.Input);
+    }
+
+    public static IPipeable<TInput, TOutput> Everything<TInput, TOutput>(IPipeable<TInput, TOutput> pipeable)
+    {
+        if (pipeable == null) throw new ArgumentNullException(nameof(pipeable));
+        return new PipeableCache<TInput, TOutput>(pipeable, CacheFlags.Input & CacheFlags.Output);
     }
 }
