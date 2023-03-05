8d4ddaccc4c2ee734450d8e0bf57a83fb989a4d0
dvolper <github@david.vollmers.org>
2023-03-05 17:07:38 +0100

    tests

--- tests/Pipes.Tests/Caching/CacheIntegrationTests.cs
+++ tests/Pipes.Tests/Caching/CacheIntegrationTests.cs
@@ -6,6 +6,31 @@ namespace Pipes.Tests.Caching;
 public class CacheIntegrationTests
 {
     [Fact]
+    public void Test_Execute_CacheInput()
+    {
+        const int input = 4;
+        var pipeable = new DelegatePipeable<int, int>(i => (int) i!, p => p.Pipe(p.Input * 2));
+        var pipe = new Pipe<int, int>
+        {
+            Cache.Input(pipeable)
+        };
+        
+        var result1 = pipe.Execute(input);
+        Assert.Equal(8, result1);
+        Assert.Equal(result1, pipe.Output);
+
+        var result2 = pipe.Execute(result1);
+        Assert.Equal(result1, result2);
+        Assert.Equal(result1, pipe.Output);
+        
+        pipe.Reset();
+
+        var result3 = pipe.Execute(result2);
+        Assert.Equal(16, result3);
+        Assert.Equal(result3, pipe.Output);
+    }
+    
+    [Fact]
     public void Test_Execute_CacheOutput()
     {
         const int input = 4;
@@ -34,7 +59,7 @@ public class CacheIntegrationTests
     public async Task Test_ExecuteAsync_CacheOutput()
     {
         const int input = 4;
-        var pipeable = new DelegatePipeable<int, int>(i => (int) i!, p => p.Pipe(p.Input * 2));
+        var pipeable = new DelegatePipeable<int, int>(i => (int) i!, async p => await p.PipeAsync(p.Input * 2));
         var pipe = new Pipe<int, int>
         {
             Cache.Output(pipeable)
