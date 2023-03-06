a762d59e7a0bed9d151b066f1d3390633d397b7f
dvolper <github@david.vollmers.org>
2023-03-06 18:05:47 +0100

    tests

---
+++ tests/Pipes.AspNetCore.Tests/ApplicationBuilderExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Pipes.AspNetCore.Tests;
+
+public class ApplicationBuilderExtensionsTests
+{
+    [Fact]
+    public void Test_UsePipes_ApplicationBuilderIsNull()
+    {
+        var exception = Assert.Throws<ArgumentNullException>(() => ApplicationBuilderExtensions.UsePipes(null!));
+        Assert.Equal("applicationBuilder", exception.ParamName);
+    }
+
+    [Fact]
+    public void Test_UsePipes()
+    {
+        var applicationBuilder = new Mock<IApplicationBuilder>();
+
+        // ReSharper disable once InvokeAsExtensionMethod
+        ApplicationBuilderExtensions.UsePipes(applicationBuilder.Object);
+        
+        applicationBuilder.Verify(ab => ab.Use(It.IsAny<Func<RequestDelegate, RequestDelegate>>()), Times.Once);
+    }
+}
