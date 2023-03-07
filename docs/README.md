5256a3a3ad7a624269a7fefa35a5bba92853bbfc
dvolper <github@david.vollmers.org>
2023-03-07 16:58:21 +0100

    use tailwind ui template

---
+++ docs/README.md
@@ -0,0 +1,7 @@
+# Pipes Documentation
+
+## Tailwind UI Template
+
+This documentation is build on top of a Tailwind UI template which is licensed to the author of this repository.
+
+You can read more about this [here](https://tailwindui.com/).
--- docs/src/common/navigation-item.ts
+++
@@ -1,8 +0,0 @@
-import type { ForwardRefExoticComponent, SVGProps } from 'react'
-
-export interface NavigationItem {
-  readonly name: string
-  readonly icon: ForwardRefExoticComponent<SVGProps<SVGSVGElement>>
-  readonly isActive: boolean
-  readonly href: string
-}
